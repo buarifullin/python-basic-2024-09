5866fe1a65428bfe9cb0b11bd736197cfd253c83
Arif <arifullinbulat@yandex.ru>
2024-10-27 16:01:55 +0300

    Add solution for even filtering

--- homework_01/old_version/main.py
+++ homework_01/old_version/main.py
@@ -32,3 +32,5 @@ def filter_numbers(numbers_lust, filter_type):
     """
     if filter_type == ODD:
         return [number for number in numbers_lust if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_lust if number % 2 == 0]
